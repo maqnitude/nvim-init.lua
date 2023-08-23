eb714ba0fa7658ace34dd9bcb6287b578d33dd1a
maqnitude <anhquanflpro2003@gmail.com>
2023-08-23 23:51:43 +0700

    change colorscheme to 'habamax'

--- lua/maqnitude/config/init.lua
+++ lua/maqnitude/config/init.lua
@@ -1,5 +1,9 @@
 require("maqnitude.config.options")
 require("maqnitude.config.keymaps")
 
+-- Netrw settings
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
+
+-- Colorscheme
+vim.cmd.colorscheme("habamax")
