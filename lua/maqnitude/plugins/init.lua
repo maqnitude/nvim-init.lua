1ba26959874429894eff6c766b5285a5596ce813
maqnitude <anhquanflpro2003@gmail.com>
2023-08-25 23:23:02 +0700

    treesitter: add tree sitter

--- lua/maqnitude/plugins/init.lua
+++ lua/maqnitude/plugins/init.lua
@@ -1,2 +1,19 @@
 return {
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "c",
+                    "lua",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },  
+            })
+        end
+    },
 }
