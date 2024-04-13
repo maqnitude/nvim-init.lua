a6e35b6f7e694c19fe9c01d0c1b42bdd73386e12
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-04-13 22:38:44 +0700

    Editor: Changed default tab size to 2

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -21,9 +21,9 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
