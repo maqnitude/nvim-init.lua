4fb108c15d9470eef3b7ac79366c999a6a7e0335
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-04-15 17:53:54 +0700

    Options: Added tab space settings for specific filetypes

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,15 +1,13 @@
 -- GENERAL --
+
 vim.opt.mouse = "a"
-
 vim.opt.clipboard = { "unnamed", "unnamedplus" }
-
 vim.opt.termguicolors = true
-
 vim.opt.timeout = false
-
 vim.opt.completeopt = { "menu", "menuone", "preview", "noselect" }
 
 -- EDITOR --
+
 vim.opt.guicursor = "a:block-nCursor"
 vim.opt.cursorcolumn = false
 vim.opt.cursorline = true
@@ -21,10 +19,33 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+-- Tabs
+local filetype_settings = {
+    ['md'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+
+    ['html'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['htmldjango'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['css'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+
+    ['xml'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['json'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+
+    ['javascript'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['javascriptreact'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['typescript'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+    ['typescriptreact'] = { tabstop = 2, softtabstop = 2, shiftwidth = 2 },
+}
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+for filetype, settings in pairs(filetype_settings) do
+    vim.cmd(string.format("autocmd FileType %s setlocal tabstop=%s softtabstop=%s shiftwidth=%s",
+                          filetype, settings.tabstop, settings.softtabstop, settings.shiftwidth))
+end
+--
 
 vim.opt.smartindent = true
 vim.opt.wrap = false
