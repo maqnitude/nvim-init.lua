93d7ca32e9e8dd7a230c83a55777113c950dd14b
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-07-17 17:55:28 +0700

    feat: use the new default colorscheme

--- lua/configs/init.lua
+++ lua/configs/init.lua
@@ -8,3 +8,9 @@ vim.g.netrw_banner = 0
 -- Colorscheme & Highlighting
 -- vim.cmd.colorscheme("habamax")
 -- vim.cmd.highlight("MatchParen", "gui=bold")
+
+-- Transparent background
+local groups = { "Normal", "NormalFloat", "NormalNC", "VertSplit" }
+for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none" })
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,11 @@
 return {
     -- Git/Github related
-    { "tpope/vim-fugitive" },
+    {
+        "tpope/vim-fugitive",
+        cond = function ()
+            return not vim.g.vscode
+        end,
+    },
     -- {
     --     "github/copilot.vim",
     --     event = { "BufNewFile", "BufReadPost" },
@@ -9,9 +14,6 @@ return {
     -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
-        cond = function ()
-            return not vim.g.vscode
-        end,
         build = ":TSUpdate",
         event = { "BufNewFile", "BufReadPost" },
         opts = function ()
@@ -19,37 +21,94 @@ return {
         end,
         config = function (_, opts)
             require("nvim-treesitter.configs").setup(opts)
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
     -- LSP
     {
+        "williamboman/mason.nvim",
+        event = { "VimEnter" },
+        config = function ()
+            require("mason").setup({
+                ui = {
+                    border = "single",
+                },
+            })
+        end,
+        cond = function ()
+            return not vim.g.vscode
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = { "VimEnter" },
+        opts = function ()
+            return require("plugins.configs.mason_lspconfig")
+        end,
+        config = function (_, opts)
+            local mason_lspconfig = require("mason-lspconfig")
+            mason_lspconfig.setup(opts.setup)
+            mason_lspconfig.setup_handlers(opts.handlers)
+        end,
+        cond = function ()
+            return not vim.g.vscode
+        end
+    },
+    {
         "neovim/nvim-lspconfig",
+        event = { "VimEnter" },
         dependencies = {
-            {
-                "williamboman/mason.nvim",
-                config = function ()
-                    require("mason").setup({
-                        ui = {
-                            border = "single",
-                        },
-                    })
-                end
-            },
-            {
-                "williamboman/mason-lspconfig.nvim",
-                opts = function ()
-                    return require("plugins.configs.mason_lspconfig")
-                end,
-                config = function (_, opts)
-                    local mason_lspconfig = require("mason-lspconfig")
-                    mason_lspconfig.setup(opts.setup)
-                    mason_lspconfig.setup_handlers(opts.handlers)
-                end
-            },
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim"
         },
         config = function ()
             require("plugins.configs.lspconfig")
+        end,
+        cond = function ()
+            return not vim.g.vscode
+        end
+    },
+
+    -- Debugger
+    {
+        "mfussenegger/nvim-dap",
+        config = function ()
+            require("plugins.configs.dap.adapters")
+            require("plugins.configs.dap.configurations")
+        end,
+        cond = function ()
+            return not vim.g.vscode
+        end
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        event = "VeryLazy",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio"
+        },
+        config = function ()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            dap.listeners.before.attach.dapui_config = function()
+              dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+              dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+              dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+              dapui.close()
+            end
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
@@ -59,6 +118,9 @@ return {
         event = { "InsertEnter" },
         config = function ()
             require("nvim-autopairs").setup()
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
@@ -74,10 +136,14 @@ return {
     -- Indent guides
     {
         "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost" },
         main = "ibl",
         opts = {},
         config = function ()
             require("ibl").setup()
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
@@ -98,24 +164,32 @@ return {
         },
         config = function ()
             require("plugins.configs.cmp")
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
     -- Telescope
     {
         "nvim-telescope/telescope.nvim", tag = "0.1.2",
+        event = { "VimEnter" },
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
         config = function ()
             require("telescope").setup()
             require("plugins.configs.telescope")
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
     -- Statusline
     {
         "nvim-lualine/lualine.nvim",
+        event = { "BufReadPost" },
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
@@ -126,39 +200,36 @@ return {
                     theme = "auto",
                 }
             })
+        end,
+        cond = function ()
+            return not vim.g.vscode
         end
     },
 
     -- Colorscheme
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function ()
-            require("tokyonight").setup({
-                style = "night",
-                styles = {
-                    comments = { italic = false },
-                },
-            })
-
-            vim.cmd [[ colorscheme tokyonight ]]
-
-            -- Transparent background
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        end
-    },
-
     -- {
-    --     "bluz71/vim-moonfly-colors",
-    --     name = "moonfly",
+    --     "folke/tokyonight.nvim",
     --     lazy = false,
     --     priority = 1000,
+    --     opts = {},
     --     config = function ()
-    --         vim.cmd.colorscheme("moonfly")
-    --         vim.cmd.highlight({"Comment", "cterm=NONE", "gui=NONE"})
+    --         require("tokyonight").setup({
+    --             style = "night",
+    --             styles = {
+    --                 comments = { italic = false },
+    --             },
+    --         })
+    --
+    --         vim.cmd [[ colorscheme tokyonight ]]
+    --
+    --         -- Transparent background
+    --         local groups = { "Normal", "NormalFloat", "NormalNC", "VertSplit" }
+    --         for _, group in ipairs(groups) do
+    --             vim.api.nvim_set_hl(0, group, { bg = "none" })
+    --         end
+    --     end,
+    --     cond = function ()
+    --         return not vim.g.vscode
     --     end
-    -- }
+    -- },
 }
