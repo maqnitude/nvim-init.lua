b73545e8bbc736814b1c08e38ef63181490d6d17
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-11-23 11:34:49 +0700

    Update general settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     config = function()
         vim.g.nord_contrast = true
-        -- vim.g.nord_disable_background = true
+        vim.g.nord_disable_background = true
         vim.g.nord_italic = false
 
         require("nord").set()
