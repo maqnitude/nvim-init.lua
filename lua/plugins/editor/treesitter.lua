eb7efdc19ebe0fce04878ecb694e9ad8b5606601
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-12-12 10:53:57 +0700

    chore(treesitter): add more parsers

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -3,30 +3,35 @@ return {
         "nvim-treesitter/nvim-treesitter",
         event = { "BufReadPost", "BufNewFile" },
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                -- core
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = {
+                    -- essentials
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "markdown",
+                    "markdown_inline",
 
-                -- standards
-                "cpp",
-                "c_sharp",
-                "python",
+                    -- standards
+                    "cpp",
+                    "c_sharp",
+                    "python",
 
-                -- web & others
-                "html",
-                "css",
-                "json",
-                "javascript",
-                "typescript",
-                "tsx", -- for React JSX/TSX
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        }
+                    -- web & others
+                    "html",
+                    "css",
+                    "json",
+                    "xml",
+                    "javascript",
+                    "typescript",
+                    "tsx", -- for React JSX/TSX
+                },
+                sync_install = true,
+                highlight = { enable = true },
+            }
+        end
     }
 }
