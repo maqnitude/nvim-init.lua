d8eaca5b7b57f3f109051e2a02d006ed731aca4d
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2025-03-08 12:41:50 +0700

    feat(plugins): add standalone entry for mason.nvim

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        cmd = { "Mason" },
+        opts = {
+            ui = {
+                border = "single",
+            }
+        }
+    },
+}
